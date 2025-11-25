26744c988d47f75214e6dc98c9fc53f6a8846f70
hosu kang <rkdghtn6036@gmail.com>
2025-11-25 23:37:54 +0900

    Fix Netlify MIME type issue

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          konva: ['konva', 'react-konva'],
+          gsap: ['gsap'],
+        },
+      },
+    },
+  },
+})
+
